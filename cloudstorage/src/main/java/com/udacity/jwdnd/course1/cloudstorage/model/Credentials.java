913bf0f2d98d11a3460f6f53684a06d0fd8c2f3f
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-03-14 03:06:53 -0400

    Creating 'Credentials' , 'Files' and 'Notes' classes in 'model' package to match all tables in database schema 'schema.sql'.

---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Credentials.java
@@ -0,0 +1,59 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+public class Credentials {
+    private Integer credentialId;
+    private String url;
+    private String username;
+    private String key;
+    private String password;
+    private Integer userId;
+    //foreign key (userid) references USERS(userid)
+
+    public Integer getCredentialId() {
+        return credentialId;
+    }
+
+    public void setCredentialId(Integer credentialId) {
+        this.credentialId = credentialId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Notes.java
@@ -0,0 +1,41 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+public class Notes {
+    private Integer noteId;
+    private String noteTitle;
+    private String noteDescription;
+    private Integer userId;
+    //foreign key (userid) references USERS(userid)
+
+    public Integer getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(Integer noteId) {
+        this.noteId = noteId;
+    }
+
+    public String getNoteTitle() {
+        return noteTitle;
+    }
+
+    public void setNoteTitle(String noteTitle) {
+        this.noteTitle = noteTitle;
+    }
+
+    public String getNoteDescription() {
+        return noteDescription;
+    }
+
+    public void setNoteDescription(String noteDescription) {
+        this.noteDescription = noteDescription;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
