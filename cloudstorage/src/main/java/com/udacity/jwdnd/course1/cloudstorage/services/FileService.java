14ddebe3d07af2b09be8bceccec146fe50a4109b
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-03-30 16:28:53 -0400

    Adding @Controller to FileController class and @Service to FileSservice class.

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
@@ -2,11 +2,13 @@ package com.udacity.jwdnd.course1.cloudstorage.services;
 
 import com.udacity.jwdnd.course1.cloudstorage.mapper.FileMapper;
 import com.udacity.jwdnd.course1.cloudstorage.model.Files;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
 
+@Service
 public class FileService {
 
     private FileMapper fileMapper;
