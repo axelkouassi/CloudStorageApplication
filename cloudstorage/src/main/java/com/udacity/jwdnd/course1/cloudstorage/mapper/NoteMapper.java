cbd0e0284be17df60944a1c80691b0f96353fa6d
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-04-22 11:05:42 -0400

    Creation of NoteMapper interface.

---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -0,0 +1,33 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.Notes;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface NoteMapper {
+
+    @Insert("INSERT INTO NOTES (notetitle, notedescription, userid) " +
+            "VALUES(#{noteTitle}, #{noteDescription}, #{userId})")
+    @Options(useGeneratedKeys = true, keyProperty = "noteid")
+    int insert (Notes note);
+
+    @Update("UPDATE NOTES set notetitle= #{noteTitle}, notedescription = #{noteDescription}, " +
+            "userid = #{userId} WHERE noteid = #{noteId}")
+    int update(Notes note);
+
+    @Delete("DELETE FROM NOTES WHERE noteid = #{noteId}")
+    void delete(Integer noteId);
+
+    @Select("SELECT * FROM NOTES")
+    List<Notes> getAllNotes();
+
+    @Select("SELECT * FROM NOTES WHERE noteid = #{noteId}")
+    List<Notes> getNoteByNoteId(Integer noteId);
+
+    @Select("SELECT * FROM NOTES WHERE userid = #{userId}")
+    List<Notes> getNotesByUserId(Integer userId);
+
+
+}
