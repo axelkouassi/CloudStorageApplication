759b43ac06d1f56c8afcc02ae72b12082190b7b6
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-04-22 12:29:45 -0400

    Creation of NoteService class.

---
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/NoteService.java
@@ -0,0 +1,36 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.NoteMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.Notes;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NoteService {
+    private NoteMapper noteMapper;
+
+    public int createNote(Notes note) {
+        return noteMapper.insert(note);
+    }
+
+    public List<Notes> getAllNotes() {
+        return this.noteMapper.getAllNotes();
+    }
+
+    public List<Notes> getNotesByUserId(Integer userId) {
+        return this.noteMapper.getNotesByUserId(userId);
+    }
+
+    public List<Notes> getNoteByNoteId(Integer noteId) {
+        return this.noteMapper.getNoteByNoteId(noteId);
+    }
+
+    public void deleteNote(Notes note, Integer id) {
+        this.noteMapper.delete(id);
+    }
+
+    public void editNote(Notes note) {
+        this.noteMapper.update(note);
+    }
+}
