b7df2ee4031f67e2f0d9d4319dd16b48c1d930ca
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-05-02 00:50:40 -0400

    Error in pom.xml with spring-boot-maven-plugin not found.

--- cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/security/SecurityConfig.java
+++ cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/security/SecurityConfig.java
@@ -1,15 +1,11 @@
 package com.udacity.jwdnd.course1.cloudstorage.security;
 
 import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationService;
-import com.udacity.jwdnd.course1.cloudstorage.services.EncryptionService;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AndRequestMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
